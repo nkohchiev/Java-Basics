34dc13e5a3e38a34347d808478372ff858eda039
Ned <j0h@example.com>
2021-03-12 11:00:14 +0000

    updated

---
+++ src/FirstStepsInProgramming/CurrencyConverterUSDToBGN.java
@@ -0,0 +1,14 @@
+package FirstStepsInProgramming;
+
+import java.util.Scanner;
+
+public class CurrencyConverterUSDToBGN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double USD = Double.parseDouble(scanner.nextLine());
+        double BGN = USD * 1.79549;
+
+        System.out.println(BGN);
+    }
+}
